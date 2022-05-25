0e96e045b0fc9ca96c069e45906590784dd3daea
ChaeHo95 <chlcogh11@naver.com>
2022-05-26 00:20:01 +0900

    코딩테스트 신고 결과 받기
    실패의 연속

--- codingtest/src/CodingTest1.java
+++ codingtest/src/CodingTest1.java
@@ -1,36 +1,67 @@
 import java.util.*;
-
+import java.util.stream.Collectors;
 
 public class CodingTest1 {
-    public int[] solution(String[] id_list, String[] report, int k) {
+    public int[] beforSolution1(String[] id_list, String[] report, int k) { // 8.3
         int[] answer = {};
         answer = new int[id_list.length];
-        HashMap<String,HashSet<String>> report_content = new HashMap<>();
+        HashMap<String, HashSet<String>> report_content = new HashMap<>();
         HashMap<String, Integer> count = new HashMap<>();
 
-        for(String name : id_list){
-            report_content.put(name,new HashSet<>());
+        for (String name : id_list) {
+            report_content.put(name, new HashSet<>());
         }
-        for(String reported : report){
+        for (String reported : report) {
             String[] reported_names = reported.split(" ");
             report_content.get(reported_names[0]).add(reported_names[1]);
-            if(count.containsKey(reported_names[1])){
-                count.put(reported_names[1],count.get(reported_names[1])+1);
-            }else {
-                count.put(reported_names[1],1);
+            if (count.containsKey(reported_names[1])) {
+                count.put(reported_names[1], count.get(reported_names[1]) + 1);
+            } else {
+                count.put(reported_names[1], 1);
             }
         }
-        for(int i=0; i<id_list.length; i++){
-            for (String reported:report_content.get(id_list[i])) {
-                if(count.get(reported) == k){
+        for (int i = 0; i < id_list.length; i++) {
+            for (String reported : report_content.get(id_list[i])) {
+                if (count.get(reported) == k) {
                     answer[i]++;
                 }
             }
         }
 
         return answer;
-
     }
+    public int[] beforSolution2(String[] id_list, String[] report, int k) { // 12.5
+        int[] answer = new int[id_list.length];
+
+        for(int i=0; i<id_list.length; i++){
+            int index = i;
+            Arrays.asList(report).stream().distinct().filter(name -> name.contains(id_list[index] + " ")).forEach(counts ->{
+                String reporter = counts.replaceAll(id_list[index] + " ","");
+                if (Arrays.asList(report).stream().distinct().filter(name -> name.contains(" "+reporter)).count() == k){
+                    answer[index]++;
+                };
+            });
+        }
+        return answer;
+    }
+
+    public int[] solution(String[] id_list, String[] report, int k) {
+        List<String> reportDocuments = Arrays.stream(report).distinct().collect(Collectors.toList());
+        Map<String,Integer> counts = new HashMap<>();
+        for(String name : reportDocuments){
+            String reportUser = name.split(" ")[1];
+            counts.put(reportUser,counts.getOrDefault(reportUser,0)+1);
+            // 찾는 키가 존재한다면 찾는 키의 값을 반환하고 없다면 기본 값을 반환하는 메서드
+        }
+
+        return Arrays.stream(id_list).map(name -> {
+            String user = name;
+            List<String> reportMaps = reportDocuments.stream().filter(reported -> reported.startsWith(user+" ")).collect(Collectors.toList());
+            return reportMaps.stream().filter(reportMap-> counts.getOrDefault(reportMap.split(" ")[1],0) >=k).count();
+        }).mapToInt(Long::intValue).toArray();
+    }
+
+
     public static void main(String[] args) {
         CodingTest1 codingTest1 = new CodingTest1();
 
