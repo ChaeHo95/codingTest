0b1fe2a85b4dd005ea096e9215bb861cf9eccd57
ChaeHo95 <chlcogh11@naver.com>
2022-06-03 01:35:03 +0900

    선택적 정렬로 순수 for문 변경

--- codingtest/src/CodingTest5.java
+++ codingtest/src/CodingTest5.java
@@ -9,17 +9,20 @@ class CodingTest5{
         for (int i=0; i< numbers.length; i++) {
             testNums[i] = String.valueOf(numbers[i]);
         }
-//        for(int i=0; i < testNums.length; i++){
-//            for(int j= i+1; j < testNums.length; j++){
-//                String temp = "";
-//                if(Integer.parseInt(testNums[i]+testNums[j]) < Integer.parseInt(testNums[j] + testNums[i])){
-//                    temp = testNums[i];
-//                    testNums[i] = testNums[j];
-//                    testNums[j] = temp;
-//                }
-//            }
-//
-//        }
+
+        String temp = "";
+        for(int i=0; i < testNums.length-1; i++){
+            int index = i;
+            for(int j= i+1; j < testNums.length; j++){
+                if(Integer.parseInt(testNums[index]+testNums[j]) < Integer.parseInt(testNums[j] + testNums[index])){
+                    index = j;
+                }
+                temp = testNums[i];
+                testNums[i] = testNums[index];
+                testNums[index] = temp;
+            }
+
+        }
 //        Arrays.sort(testNums, new Comparator<String>() {
 //            // sort()메소드를 이용하여 순차적으로 비교하여 정렬
 //            // sort()메소드의 인자 중  Comparator부분을 재정의할 수 있어
@@ -35,15 +38,15 @@ class CodingTest5{
 //                // a가 b보다 작으면 앞쪽으로
 //            }
 //        });
-        Arrays.sort(testNums, (o1, o2) -> {
-            String a = o2 + o1;
-            String b = o1 + o2;
-            return a.compareTo(b);
-            // compareTo()함수는 두개의 값을 비교하여 int 값으로 반환해주는 함수
-            // a가 b보다 크면 뒤쪽으로
-            // a가 b랑 같으면 그대로
-            // a가 b보다 작으면 앞쪽으로
-        });
+//        Arrays.sort(testNums, (o1, o2) -> {
+//            String a = o2 + o1;
+//            String b = o1 + o2;
+//            return a.compareTo(b);
+//            // compareTo()함수는 두개의 값을 비교하여 int 값으로 반환해주는 함수
+//            // a가 b보다 크면 뒤쪽으로
+//            // a가 b랑 같으면 그대로
+//            // a가 b보다 작으면 앞쪽으로
+//        });
 //        for(String test :testNums){
 //            answer += test;
 //        }
